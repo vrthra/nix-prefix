73cd41f0dadd64cd18b418b32ab3742f3bb88181
Rahul <gopinath@eecs.oregonstate.edu>
2016-07-27 12:24:45 -0700

    update

---
+++ bin/remove-glibc-hack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+./bin/check-nss.py
+nixdir=$(nix-build $NIXPKGS -A glibc.out)/lib
+chmod 777 $nixdir
+#libnss=$(find ../nix -name libnss_sss.so.\* | grep sssd)
+#echo $libnss
+#cp -f $libnss $nixdir/
+rm $nixdir/libnss_sss.so.*
+chmod 555 $nixdir
+./bin/check-nss.py
